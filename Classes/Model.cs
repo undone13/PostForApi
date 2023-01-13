65261118473253e65e097bc3043092dfca7329fd
bogdan13 <bogdanichim04@gmail.com>
2023-01-13 16:22:52 +0200

    Add project files.

---
+++ Classes/Model.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PostForApi.Classes
+{
+    public class Model
+    {
+        public string DataBaseName { get; set; }
+
+        public Model()
+        {
+            Headers = new List<Header>();
+            Values = new List<List<ValueClass>>();
+        }
+
+        public List<Header> Headers { get; set; }
+
+        public List<List<ValueClass>> Values { get; set; }
+    }
+
+    public class Header
+    {
+        public string ColumnName { get; set; }
+        public string Type { get; set; }
+        public int Size { get; set; }
+    }
+
+    public class ValueClass
+    {
+        public string ColumnName { get; set; }
+        public Object Value { get; set; }
+    }
+
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿using PostForApi.Classes;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Windows;
+
+namespace PostForApi
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        //VARIABLES
+        private DataTable dataTable = new DataTable();
+
+        private void ButtonStart_Click(object sender, RoutedEventArgs e)
+        {
+            SqlConnectionStringBuilder connectionString = new SqlConnectionStringBuilder()
+            {
+                DataSource = "localhost",
+                IntegratedSecurity = true,
+                InitialCatalog = "FreyaRestaurant222"
+            };
+            Debug.WriteLine(connectionString.ToString());
+
+            using (SqlConnection con = new SqlConnection(connectionString.ToString()))
+            {
+                try
+                {
+                    con.Open();
+                }
+                catch(Exception ex)
+                {
+                    Debug.WriteLine("Connection error: " + ex.Message);
+                }
+
+                string commandText = "select top 100 id, startDate, finishDate, paidDate, totalValueWithVat from STOrder";
+                SqlCommand command = new SqlCommand(commandText, con);
+
+                try
+                {
+                    SqlDataAdapter da = new SqlDataAdapter(command);
+                    da.Fill(dataTable);
+                    con.Close();
+                    da.Dispose();
+                }
+                catch(Exception ex)
+                {
+                    Debug.Write("Command error: " + ex.Message);
+                }
+            }
+
+            Model model = new Model();
+
+            for(int i = 0; i < dataTable.Columns.Count; i++)
+            {
+                Header header = new Header();
+                header.ColumnName = dataTable.Columns[i].ColumnName;
+                header.Size = 100;
+                header.Type = dataTable.Columns[i].DataType.ToString();
+
+                model.Headers.Add(header);
+            }
+
+            for(int i = 0; i < dataTable.Rows.Count; i++)
+            {
+                
+            }
+
+        }
+    }
+}
